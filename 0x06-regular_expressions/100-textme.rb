c362c0e1dca2329622d8e82c3504f063b065de5c
ngari-kev <kevngariwangui@gmail.com>
2024-03-16 12:02:50 +0000

    100-textme.rb

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+#define the RegEx pattern
+PATTERN = /\[from:([\w\s+:]+)\] \[to:(\+\d+)\] \[flags:([^]]+)\]/
+
+# Extract sender, receiver, and flags
+def extract_info(log_entry)
+    matches = log_entry.scan(PATTERN)
+    if matches.any?
+        sender = matches[0][0]
+        receiver = matches[0][1]
+        flags = matches[0][2]
+        puts "#{sender},#{receiver},#{flags}"
+    end
+end
+
+# Main- processes input and calls extract_info()
+if ARGV.length == 1
+    log_entry = ARGV[0]
+    extract_info(log_entry)
+end
