0bb6c4703807ffd0547d5531d16e6ac37e24b151
ngari-kev <kevngariwangui@gmail.com>
2024-08-21 15:05:53 +0300

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
-# Enhance Nginx server capacity by increasing the traffic limit
+#Enhance Nginx server capacity by increasing the traffic limit
 
 # Adjust the ULIMIT in the Nginx default configuration
-exec { 'fix--for-nginx':
+exec { 'update-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# Restart the Nginx sevice to apply changes
+# Restart the Nginx service to apply changes
 -> exec { 'reload-nginx-service':
   command => '/etc/init.d/nginx restart',
   path    => '/sbin/:/bin/:/usr/sbin/:/usr/bin/'
 }
-
